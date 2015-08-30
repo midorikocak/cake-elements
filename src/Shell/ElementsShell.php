6994b52bc13b44b96fa37b2b997e9840a0e52bad
Midori Koçak <mtkocak@gmail.com>
2015-08-30 21:44:15 +0200

    added

--- src/Shell/ElementsShell.php
+++ src/Shell/ElementsShell.php
@@ -2,6 +2,8 @@
 namespace CakeElements\Shell;
 
 use Cake\Console\Shell;
+use Cake\Filesystem\Folder;
+use Cake\Filesystem\File;
 
 /**
  * Elements shell command.
@@ -16,11 +18,32 @@ class ElementsShell extends Shell
      */
     public function main()
     {
-        $this->out('Hello world.');
     }
 
     public function heyThere($name = 'Anonymous')
     {
         $this->out('Hey there ' . $name);
+    }
+    
+    public function install($name)
+    {
+        $address = explode('/',$name);
+        
+        $user = $address[0];
+        $repo = $address[1];
+        
+        $elementDir = new Folder(APP.DS.'Template'.DS.'Element', true, 0755);
+        
+        $userDir = new Folder(APP.DS.'Template'.DS.'Element'.DS.$user, true, 0755);
+        
+        $repoDir = new Folder(APP.DS.'Template'.DS.'Element'.DS.$user.DS.$repo.DS, true, 0755);
+        
+
+        $client = new \Github\Client();
+        $contents = $client->api('repo')->contents()->show($user,$repo);
+        foreach($contents as $content){
+            $file = file_get_contents($content['download_url']);
+            $this->createFile(APP.DS.'Layout'.DS.'Element'.DS.$user.DS.$repo.DS.$content['name'],$file);
+        }
     }
 }
